c06558d51bd3c531cd333e4d16b88bd4a8fcf121
mhmmttnbl <muhammet.tonbul@outlook.com>
2024-04-15 17:20:06 +0300

    [feat](rtqt/core): core_maximum_element procedure added at core_algorithm.h.

--- rtqt/core/core_algorithm.h
+++ rtqt/core/core_algorithm.h
@@ -26,6 +26,25 @@ namespace rtqt
 
     return __tmp;
   }
+
+  template <typename _ForwardIterator>
+  inline _ForwardIterator
+  core_maximum_element(_ForwardIterator __first, _ForwardIterator __last)
+  {
+    if (__first == __last) {
+      return __last;
+    }
+
+    _ForwardIterator __tmp = __first;
+
+    while (++__first != __last) {
+      if (*__first > *__tmp) {
+        __tmp = __first;
+      }
+    }
+
+    return __tmp;
+  }
 } // namespace rtqt
 
 #endif /* !CORE_ALGORITHM_H */
